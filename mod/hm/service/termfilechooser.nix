d5d3636cb00c9849e1b4429af3c1ab0b06eaefb7
nyxmeowmeow <rraaiin@proton.me>
2025-11-20 10:27:42 +1000

    20/11 10:27:42

--- mod/hm/service/termfilechooser.nix
+++ mod/hm/service/termfilechooser.nix
@@ -20,7 +20,8 @@
   home.file.".config/xdg-desktop-portal-termfilechooser/config".text = /* ini */ ''
 [filechooser]
 cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
-env=TERMCMD=foot --app-id="foot.yazi.filechooser"
+default_dir=$HOME
+env=TERMCMD=foot --hold --app-id="foot.yazi.filechooser"
   '';
 
 
