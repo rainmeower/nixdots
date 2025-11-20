3b798494c5ada20aad7b374fb7f9f799c6e6a600
nyxmeowmeow <rraaiin@proton.me>
2025-11-20 16:22:28 +1000

    20/11 16:22:28

--- mod/hm/tui/nixvim/util/gx.nix
+++ mod/hm/tui/nixvim/util/gx.nix
@@ -23,8 +23,6 @@ require('gx-extended').setup({
   -- Optional: GitHub file line permalinks
   enable_github_file_line = true,
 
-  -- Optional: Custom browser
-  open_fn = require('lazy.util').open,
 })
     '';
   };
