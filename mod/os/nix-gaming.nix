5d87e8b02872cbb9e27f925ab396b472e38d8a0e
nyxmeowmeow <rraaiin@proton.me>
2025-11-21 14:13:21 +1000

    21/11 14:13:21

--- mod/os/nix-gaming.nix
+++ mod/os/nix-gaming.nix
@@ -3,7 +3,7 @@
   ...
 }: {
   imports = [
-    # inputs.nix-gaming.nixosModules.pipewireLowLatency
+    inputs.nix-gaming.nixosModules.pipewireLowLatency
     inputs.nix-gaming.nixosModules.platformOptimizations
   ];
 }
