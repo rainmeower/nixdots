2cfa25d23fab0b26a21286a5f7cbd63a3edae8bf
nyxmeowmeow <rraaiin@proton.me>
2025-11-21 20:58:48 +1000

    21/11 20:58:48

--- theme/quiet/hm/default.nix
+++ theme/quiet/hm/default.nix
@@ -2,21 +2,21 @@
   imports = [
     # ../palette.nix
 
-    ./btop_kanso.nix
-    ./mpv_kanso.nix
-    ./gtk_kanso.nix
-    ./kitty_kanso.nix
-    ./qt_kanso.nix
-    ./dunst_kanso.nix
-    ./fastfetch_kanso.nix
-    ./foot_kanso_ink.nix
-    ./foot_kanso_mist.nix
-    ./foot_kanso_zen.nix
-    # ./hm/hyprpaper_kanso.nix
-    ./niri_kanso.nix
-    ./nushell_kanso.nix
-    ./starship_kanso.nix
-    ./walker_kanso.nix
-    ./yazi_kanso.nix
+    ./btop_quiet.nix
+    ./mpv_quiet.nix
+    ./gtk_quiet.nix
+    ./kitty_quiet.nix
+    ./qt_quiet.nix
+    ./dunst_quiet.nix
+    ./fastfetch_quiet.nix
+    ./foot_quiet_ink.nix
+    ./foot_quiet_mist.nix
+    ./foot_quiet_zen.nix
+    # ./hm/hyprpaper_quiet.nix
+    ./niri_quiet.nix
+    ./nushell_quiet.nix
+    ./starship_quiet.nix
+    ./walker_quiet.nix
+    ./yazi_quiet.nix
   ];
 }
