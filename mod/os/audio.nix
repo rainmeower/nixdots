0d6fdde82c67cb0918b6618b57b1e0cd5b750702
nyxmeowmeow <rraaiin@proton.me>
2025-11-21 14:33:40 +1000

    21/11 14:33:40

--- mod/os/audio.nix
+++ mod/os/audio.nix
@@ -4,16 +4,16 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # jack.enable = true;
+    jack.enable = true;
 
     # nix-gaming
-    # lowLatency = {
-    #   enable = true;
-    #   quantum = 64;
-    #   rate = 48000;
-    # };
+    lowLatency = {
+      enable = true;
+      quantum = 64;
+      rate = 48000;
+    };
   };
 
   # make pipewire realtime-capable
-  # security.rtkit.enable = true;
+  security.rtkit.enable = true;
 }
