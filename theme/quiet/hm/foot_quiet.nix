820495e5712371c6d199e0926c8a0c66613d83e9
nyxmeowmeow <rraaiin@proton.me>
2025-11-22 14:58:57 +1000

    22/11 14:58:57

--- theme/quiet/hm/foot_quiet.nix
+++ theme/quiet/hm/foot_quiet.nix
@@ -19,24 +19,24 @@ in {
           foreground = n col.fg;
 
           ## Normal/regular colors (color palette 0-7)
-          regular0 = "393B44";  # black
-          regular1 = "C34043";  # red
-          regular2 = "98BB6C";  # green
-          regular3 = "DCA561";  # yellow
-          regular4 = "7FB4CA";  # blue
-          regular5 = "938AA9";  # magenta
-          regular6 = "8ea4a2";  # cyan
-          regular7 = "f2f1ef";  # white
+          regular0 = n col.black;
+          regular1 = n col.red;
+          regular2 = n col.green;
+          regular3 = n col.yellow;
+          regular4 = n col.violet; # blue
+          regular5 = n col.pink;   # magenta
+          regular6 = n col.cyan;
+          regular7 = n col.white;
 
           ## Bright colors (color palette 8-15)
-          bright0 = "393B44";   # bright black
-          bright1 = "C34043";   # bright red
-          bright2 = "98BB6C";   # bright green
-          bright3 = "DCA561";   # bright yellow
-          bright4 = "7FB4CA";   # bright blue
-          bright5 = "938AA9";   # bright magenta
-          bright6 = "8ea4a2";   # bright cyan
-          bright7 = "f2f1ef";   # bright white
+          bright0 = n col.black_b;
+          bright1 = n col.red_b;
+          bright2 = n col.green_b;
+          bright3 = n col.yellow_b;
+          bright4 = n col.violet_b; # blue
+          bright5 = n col.pink_b;   # magenta
+          bright6 = n col.cyan_b;
+          bright7 = n col.white_b;
         };
         tweak.box-drawing-base-thickness = "0.05";
       };
