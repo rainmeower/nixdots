86678906c7a6ff7be20535e6f772462468e226fb
nyxmeowmeow <rraaiin@proton.me>
2025-11-23 22:30:29 +1000

    23/11 22:30:29

--- mod/hm/tui/nixvim/util/bracketed.nix
+++ mod/hm/tui/nixvim/util/bracketed.nix
@@ -1,29 +1,74 @@
 {
+  lib,
+  ...
+}: let
+  inherit (lib.nixvim) emptyTable;
+in {
   programs.nixvim.plugins.mini.modules.bracketed = {
     enable = true;
 
-    # lazyLoad.enable = true;
-    # lazyLoad.settings.event = [
-    #   "DeferredUIEnter"
-    # ];
+    lazyLoad.enable = true;
+    lazyLoad.settings.event = [
+      "DeferredUIEnter"
+    ];
 
     settings = {
-      keymaps = [
-      { buffer = "r"; }
-      { comment = "w"; }
-      { conflict = "x"; }
-      { diagnostic = "t"; }
-      { file = "f"; }
-      { indent = "l"; }
-      { jump = "s"; }
-      { location = "p"; }
-      { oldfile = "h"; }
-      { quickfix = "q"; }
-      { treesitter = "b"; }
-      { undo = "u"; }
-      { window = "v"; }
-      { yank = "y"; }
-      ];
+      buffer = {
+        options = emptyTable;
+        suffix = "r";
+      };
+      comment = {
+        options = emptyTable;
+        suffix = "w";
+      };
+      conflict = {
+        options = emptyTable;
+        suffix = "x";
+      };
+      diagnostic = {
+        options = emptyTable;
+        suffix = "t";
+      };
+      file = {
+        options = emptyTable;
+        suffix = "f";
+      };
+      indent = {
+        options = emptyTable;
+        suffix = "c";
+      };
+      jump = {
+        options = emptyTable;
+        suffix = "s";
+      };
+      location = {
+        options = emptyTable;
+        suffix = "p";
+      };
+      oldfile = {
+        options = emptyTable;
+        suffix = "h";
+      };
+      quickfix = {
+        options = emptyTable;
+        suffix = "q";
+      };
+      treesitter = {
+        options = emptyTable;
+        suffix = "b";
+      };
+      undo = {
+        options = emptyTable;
+        suffix = "u";
+      };
+      window = {
+        options = emptyTable;
+        suffix = "e";
+      };
+      yank = {
+        options = emptyTable;
+        suffix = "y";
+      };
     };
   };
 }
