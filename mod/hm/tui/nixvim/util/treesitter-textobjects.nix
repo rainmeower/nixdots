f75e6214cb3446cb7ce5811f770ed892481997f7
nyxmeowmeow <rraaiin@proton.me>
2025-11-24 01:58:47 +1000

    24/11 01:58:47

--- mod/hm/tui/nixvim/util/treesitter-textobjects.nix
+++ mod/hm/tui/nixvim/util/treesitter-textobjects.nix
@@ -5,28 +5,31 @@
     settings = {
       lookahead = true;
 
-      keymaps = {
-        "a=" = "@assignment.outer";
-        "i=" = "@assignment.inner";
-        "[=" = "@assignment.lhs";
-        "]=" = "@assignment.rhs";
-        aa = "@parameter.outer";
-        ia = "@parameter.inner";
-        ai = "@conditional.outer";
-        ii = "@conditional.inner";
-        "[i" = "@conditional.lhs";
-        "]i" = "@conditional.rhs";
-        al = "@loop.outer";
-        il = "@loop.inner";
-        af = "@call.outer";
-        "if" = "@call.inner";
-        am = "@function.outer";
-        im = "@function.inner";
-        ac = "@class.outer";
-        ic = "@class.inner";
-      };
       select = {
         enable = true;
+
+        keymaps = {
+          "a=" = "@assignment.outer";
+          "i=" = "@assignment.inner";
+          "[=" = "@assignment.lhs";
+          "]=" = "@assignment.rhs";
+          aa   = "@parameter.outer";
+          ia   = "@parameter.inner";
+          ai   = "@conditional.outer";
+          ii   = "@conditional.inner";
+          "[i" = "@conditional.lhs";
+          "]i" = "@conditional.rhs";
+          al   = "@loop.outer";
+          il   = "@loop.inner";
+          af   = "@call.outer";
+          "if" = "@call.inner";
+          am   = "@function.outer";
+          im   = "@function.inner";
+          ac   = "@class.outer";
+          ic   = "@class.inner";
+          ar   = "@block.outer";
+          ir   = "@block.inner";
+        };
       };
     };
   };
