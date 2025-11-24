447d7651bcccc83b40251136896fa9433326f1c3
nyxmeowmeow <rraaiin@proton.me>
2025-11-24 14:50:06 +1000

    24/11 14:50:06

--- mod/hm/tui/nixvim/util/surround.nix
+++ mod/hm/tui/nixvim/util/surround.nix
@@ -1,12 +1,12 @@
 {
-  pkgs,
-  ...
-}: {
-# doesnt work the nix way?
   programs.nixvim = {
-    extraPlugins = [ pkgs.vimPlugins.mini-surround ];
-     extraConfigLua = ''
-       require('mini.surround').setup()
-       '';
-       };
-       }
+    # extraPlugins = [ pkgs.vimPlugins.mini-surround ];
+    #  extraConfigLua = ''
+    #    require('mini.surround').setup()
+    #    '';
+    #    };
+    plugins.mini-surround = {
+      enable = true;
+    };
+  };
+}
