7ef9eee82578a6b45b661b124df0a75bb52ca7ed
nyxmeowmeow <rraaiin@proton.me>
2025-11-25 00:24:13 +1000

    25/11 00:24:13 lix :3

--- flake.nix
+++ flake.nix
@@ -3,6 +3,15 @@
 
   inputs = { # {{{
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -51,6 +60,7 @@
     self, # TODO wtf is this
     nixpkgs,
     home-manager,
+    lix-module,
     nur,
     ...
   }: let
@@ -71,6 +81,8 @@
         modules = [
           ./host/nixos/default.nix
           ./host/nixos/hm.nix
+
+          lix-module.nixosModules.default
 
           {
             nixpkgs.overlays = import ./over/default.nix;
