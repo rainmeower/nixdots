e7c50e9123871155c6871d5dc7d962bcca583c66
nyxmeowmeow <rraaiin@proton.me>
2025-11-24 22:10:35 +1000

    24/11 22:10:35

--- mod/hm/tui/nixvim/options.nix
+++ mod/hm/tui/nixvim/options.nix
@@ -53,7 +53,7 @@
     foldcolumn = "auto";
     foldmethod = "marker";
 foldtext =
-''substitute(getline(v:foldstart),'\ #\\?\ \\?{{{',''','g')'';
+''v.substitute(getline(v:foldstart),'\ #\\?\ \\?{{{',''','g')'';
 
   };
 }
