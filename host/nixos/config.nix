316fb8f5ee593430c3764b8fae75d73776b33d75
nyxmeowmeow <rraaiin@proton.me>
2025-11-24 18:12:52 +1000

    24/11 18:12:52

--- host/nixos/config.nix
+++ host/nixos/config.nix
@@ -5,13 +5,11 @@
         width = 2560;
         height = 1440;
         refresh = 165;
-        refresh_f = refresh + 0.0;
       };
       DP-3 = rec {
         width = 2560;
         height = 1440;
         refresh = 75;
-        refresh_f = refresh + 0.0;
       };
     };
   };
