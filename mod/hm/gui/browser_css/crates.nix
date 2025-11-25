74dc611b75060bb96f4521f0bf2641525e02dedf
nyxmeowmeow <rraaiin@proton.me>
2025-11-25 18:15:00 +1000

    25/11 18:15:00

--- mod/hm/gui/browser_css/crates.nix
+++ mod/hm/gui/browser_css/crates.nix
@@ -6,10 +6,7 @@
   self,
   ...
 }: let
-  # theme_trimmed = lib.strings.removeSuffix "_zen" theme;
-  theme_trimmed = builtins.split "_" theme;
-  # theme_trimmed2 = lib.lists.flatten theme_trimmed;
-  col = import "${self}/theme/${builtins.elemAt (builtins.split "_" theme) 0}/colors.nix";
+  col = import (self + "/theme/${builtins.elemAt (builtins.split "_" theme) 0}/colors.nix");
 in {
   home.file.".config/usercontent/crates.css".text = /* css */ ''
 @-moz-document domain("crates.io") {
