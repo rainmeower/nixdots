62f55ece6cb5a0269b3828d59cf3f05f747e80ae
nyxmeowmeow <rraaiin@proton.me>
2025-11-26 22:42:31 +1000

    26/11 22:42:31

--- mod/hm/tui/iamb.nix
+++ mod/hm/tui/iamb.nix
@@ -1,21 +1,18 @@
 {
-# FIXME
-  # programs.iamb = {
-  #   enable = true;
-  #   # default_profile = "meow";
-  #
-  #   settings = {
-  #     notifications.enabled = true;
-  #     image_preview.protocol = {
-  #       type = "kitty";
-  #       size = {
-  #         height = 10;
-  #         width = 66;
-  #       };
-  #     };
-  #   };
-  #
-  #     user_id = "nyxmeowmeow:matrix.org";
-  #
-  # };
+  programs.iamb = {
+    enable = true;
+    # default_profile = "meow";
+    # user_id = "nyxmeowmeow:matrix.org";
+
+    settings = {
+      notifications.enabled = true;
+      image_preview.protocol = {
+        type = "kitty";
+        size = {
+          height = 10;
+          width = 66;
+        };
+      };
+    };
+  };
 }
