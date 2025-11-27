083baa94aecfa783db6250366cb1c87a74210923
nyxmeowmeow <rraaiin@proton.me>
2025-11-28 09:02:31 +1000

    28/11 09:02:31

--- mod/hm/tui/nixvim/util/bracketed.nix
+++ mod/hm/tui/nixvim/util/bracketed.nix
@@ -1,28 +1,28 @@
 # TODO ]z for jum to fold?
 {
-  # programs.nixvim.plugins.mini-bracketed = {
-  #   enable = true;
-  #
-  #   lazyLoad.enable = true;
-  #   lazyLoad.settings.event = [
-  #     "DeferredUIEnter"
-  #   ];
-  #
-  #   settings = {
-  #     buffer.suffix = "r";
-  #     comment.suffix = "w";
-  #     conflict.suffix = "x";
-  #     diagnostic.suffix = "t";
-  #     file.suffix = "f";
-  #     indent.suffix = "c";
-  #     jump.suffix = "s";
-  #     location.suffix = "p";
-  #     oldfile.suffix = "h";
-  #     quickfix.suffix = "q";
-  #     treesitter.suffix = "b";
-  #     # undo.suffix = "u"; # disables visual block mode
-  #     window.suffix = "e";
-  #     yank.suffix = "y";
-  #   };
-  # };
+  programs.nixvim.plugins.mini-bracketed = {
+    enable = true;
+
+    lazyLoad.enable = true;
+    lazyLoad.settings.event = [
+      "DeferredUIEnter"
+    ];
+
+    settings = {
+      buffer.suffix = "r";
+      comment.suffix = "w";
+      conflict.suffix = "x";
+      diagnostic.suffix = "t";
+      file.suffix = "f";
+      indent.suffix = "c";
+      jump.suffix = "s";
+      location.suffix = "p";
+      oldfile.suffix = "h";
+      quickfix.suffix = "q";
+      treesitter.suffix = "b";
+      undo.enable = false;
+      window.suffix = "e";
+      yank.suffix = "y";
+    };
+  };
 }
