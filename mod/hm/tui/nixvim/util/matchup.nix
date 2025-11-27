2951f6e7bf007c28810795cd0bddd59a64f4c0b2
nyxmeowmeow <rraaiin@proton.me>
2025-11-28 05:21:48 +1000

    28/11 05:21:48

--- mod/hm/tui/nixvim/util/matchup.nix
+++ mod/hm/tui/nixvim/util/matchup.nix
@@ -2,19 +2,19 @@
   pkgs,
   ...
 }: {
-  programs.nixvim = {
-    extraPlugins = [ pkgs.vimPlugins.vim-matchup ];
-    extraConfigLua = ''
-    require('match-up').setup({
-      treesitter = {
-        stopline = 500
-      }
-    })
-    '';
-
-    extraConfigVim = ''
-      let g:matchup_matchparen_singleton = 0
-      let g:matchup_matchparen_offscreen = {'method': 'popup'}
-    '';
-  };
+  # programs.nixvim = {
+  #   extraPlugins = [ pkgs.vimPlugins.vim-matchup ];
+  #   extraConfigLua = ''
+  #   require('match-up').setup({
+  #     treesitter = {
+  #       stopline = 500
+  #     }
+  #   })
+  #   '';
+  #
+  #   extraConfigVim = ''
+  #     let g:matchup_matchparen_singleton = 0
+  #     let g:matchup_matchparen_offscreen = {'method': 'popup'}
+  #   '';
+  # };
 }
