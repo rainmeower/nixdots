7c82e8909cc7c0ca733aaa644e82201dd5ebcf14
nyxmeowmeow <rraaiin@proton.me>
2025-11-28 05:59:46 +1000

    28/11 05:59:46

--- mod/hm/tui/nixvim/theme/highlight.nix
+++ mod/hm/tui/nixvim/theme/highlight.nix
@@ -12,11 +12,18 @@ in {
     CursorLineNr.fg = col.accent;
     LineNr.fg = col.selection;
 
-    # MatchParen = {
-    #   fg = col.bg;
-    #   bg = col.accent;
-    #   bold = true;
-    # };
+    MatchParen = {
+      fg = col.bg;
+      bg = col.accent;
+      bold = true;
+    };
+    MatchParenCur = {
+      fg = col.bg;
+      bg = col.accent;
+      bold = true;
+    };
+
+
 
     # telescope {{{
     TelescopeBorder.fg = col.accent;
