189842f3162d2b88ce951e5e1ededb302b6827bf
nyxmeowmeow <rraaiin@proton.me>
2025-11-27 21:38:32 +1000

    27/11 21:38:32

--- mod/hm/tui/nushell.nix
+++ mod/hm/tui/nushell.nix
@@ -184,7 +184,12 @@ in {
               # }
           ]
       }
-    '';# }}}
+      $env.PROMPT_INDICATOR: ""
+      $env.PROMPT_INDICATOR_VI_NORMAL: ""
+      $env.PROMPT_INDICATOR_VI_INSERT: ""
+      # $env.PROMPT_MULTILINE_INDICATOR: The multi-line indicator
+
+    ''; # }}}
 
    #  envFile.text = /* nu */ ''
    #  '';
