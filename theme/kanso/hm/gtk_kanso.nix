a6454d663799a5249295da3521034369d6cd8a78
nyxmeowmeow <rraaiin@proton.me>
2025-12-01 17:22:20 +1000

    01/12 17:22:20

--- theme/kanso/hm/gtk_kanso.nix
+++ theme/kanso/hm/gtk_kanso.nix
@@ -2,22 +2,21 @@
   lib,
   theme,
   pkgs,
-  font_name,
   ...
 }: {
   config = lib.mkIf (lib.hasPrefix "kanso" theme) {
 
     gtk = {
       enable = true;
-      # theme = {
-      #   name = "catppuccin-macchiato-lavender-compact";
-      #   package = pkgs.catppuccin-gtk.override {
-      #     variant = "macchiato";
-      #     accents = [ "lavender" ]; # can specify multiple accents
-      #       size = "compact"; # compact, standard, large
-      #       # tweaks = [ "rimless" "black" ];
-      #   };
-      # };
+      theme = {
+        name = "catppuccin-macchiato-lavender-compact";
+        package = pkgs.catppuccin-gtk.override {
+          variant = "macchiato";
+          accents = [ "lavender" ]; # can specify multiple accents
+            size = "compact"; # compact, standard, large
+            # tweaks = [ "rimless" "black" ];
+        };
+      };
 
       iconTheme = {
         # package = pkgs.gnome.adwaita-icon-theme;
@@ -25,8 +24,8 @@
       };
 
       font = {
-        name = font_name;
-        size = 12;
+        name = "sn pro";
+        size = 11;
       };
 
       gtk3.extraConfig.gtk-key-theme-name = "Emacs";
