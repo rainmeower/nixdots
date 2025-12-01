17b15acf076aa8bbfe57d3c13f48501b30886ad6
nyxmeowmeow <rraaiin@proton.me>
2025-12-01 14:38:55 +1000

    01/12 14:38:55

--- theme/kanso/hm/yazi_kanso.nix
+++ theme/kanso/hm/yazi_kanso.nix
@@ -11,11 +11,7 @@ col = import ../colors.nix;
 in {
   config = lib.mkIf (lib.hasPrefix "kan" theme) {
     programs.yazi.theme = {
-      which = {
-        mask = {
-          bg = "";
-        };
-      };
+      which.mask.bg = "none";
       mgr = {
         cwd.fg = col.fg;
 
