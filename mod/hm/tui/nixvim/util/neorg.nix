058ffe8edf29221aca791dc59b081071250aa347
nyxmeowmeow <rraaiin@proton.me>
2025-12-03 07:59:48 +1000

    03/12 07:59:48

--- mod/hm/tui/nixvim/util/neorg.nix
+++ mod/hm/tui/nixvim/util/neorg.nix
@@ -2,7 +2,29 @@
   programs.nixvim.plugins.neorg = {
     enable = true;
 
+    lazyLoad.enable = true;
+    lazyLoad.settings.event = [
+      "DeferredUIEnter"
+    ];
+
+    telescopeIntegration.enable = true;
+
     settings = {
+      load = {
+        "core.concealer" = {
+          config = {
+            icon_preset = "basic";
+            folds = true;
+          };
+        };
+        "core.dirman" = {
+          config = {
+            workspaces = {
+              notes = "~/notes";
+            };
+          };
+        };
+      };
     };
   };
 }
