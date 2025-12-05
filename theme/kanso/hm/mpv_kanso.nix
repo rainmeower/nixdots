fe4ebfc292127ce4786f7c6b1daf35e39990b9b8
nyxmeowmeow <rraaiin@proton.me>
2025-12-05 22:10:52 +1000

    05/12 22:10:52

--- theme/kanso/hm/mpv_kanso.nix
+++ theme/kanso/hm/mpv_kanso.nix
@@ -20,7 +20,7 @@ in {
         "stats-plot_bg_border_color=f8bdb7"
         "stats-plot_bg_color=30201e"
         "stats-plot_color=f8bdb7"
-        "uosc-color=foreground=c5c9c7,foreground_text=c5c9c7,background=090e13,background_text=c5c9c7,curtain=090e13,success=98bb6c,error=e46876"
+        "uosc-color=foreground=c5c9c7,foreground_text=c5c9c7,background=090e1300,background_text=c5c9c7,curtain=090e13,success=98bb6c,error=e46876"
       ];
     };
   };
