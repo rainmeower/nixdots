3414bb303b221571dbc618ddbb99400d02ab450c
nyxmeowmeow <rraaiin@proton.me>
2025-12-06 14:32:45 +1000

    06/12 14:32:45

--- flake.nix
+++ flake.nix
@@ -65,7 +65,8 @@
     ...
   }: let
     username = "meow";
-    flake_dir = "/home/${username}/nix";
+    # flake_dir = "/home/${username}/nix";
+    flake_dir = self.outPath;
     # flake_ns_path = builtins.toString ./.; # flake nix store path
   in {
     nixosConfigurations = {
