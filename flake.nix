12727edcf557d0cdd25e67d751ebe2e2fb37c14e
nyxmeowmeow <rraaiin@proton.me>
2025-12-07 05:15:29 +1000

    07/12 05:15:29

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = { # {{{
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs_stable.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs_stable.url = "github:nixos/nixpkgs/nixos-25.05";
     lix = {
       url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
       flake = false;
