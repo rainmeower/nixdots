b7bde7ac267860cd63855fcda3dee643357ce0b4
nyxmeowmeow <rraaiin@proton.me>
2025-12-07 21:32:32 +1000

    07/12 21:32:32

--- mod/hm/tui/yazi/settings.nix
+++ mod/hm/tui/yazi/settings.nix
@@ -5,7 +5,7 @@
 }:{
   programs.yazi.settings = {
     mgr = {
-      ratio   = [ 0 3 4 ];
+      ratio   = [ 0 1 1 ];
       sort_by = "natural";
       sort_sensitive = false;
       sort_reverse 	 = false;
