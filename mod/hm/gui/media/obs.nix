75b5e0b953d0b77d0552c002a5cb67e395f83ec5
nyxmeowmeow <rraaiin@proton.me>
2025-12-08 02:47:52 +1000

    08/12 02:47:52

--- mod/hm/gui/media/obs.nix
+++ mod/hm/gui/media/obs.nix
@@ -2,12 +2,12 @@
   pkgs,
   ...
 }:{
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-vkcapture
-      obs-pipewire-audio-capture
-    ];
-  };
+  # programs.obs-studio = {
+  #   enable = true;
+  #   plugins = with pkgs.obs-studio-plugins; [
+  #     wlrobs
+  #     obs-vkcapture
+  #     obs-pipewire-audio-capture
+  #   ];
+  # };
 }
