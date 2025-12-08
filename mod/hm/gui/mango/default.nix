4c8e43fb2aab7fe60cc4f53c1432cf56e5365c26
nyxmeowmeow <rraaiin@proton.me>
2025-12-08 23:20:36 +1000

    08/12 23:20:36

--- mod/hm/gui/mango/default.nix
+++ mod/hm/gui/mango/default.nix
@@ -48,6 +48,8 @@ in {
         foot --server &
         vesktop --enable-features=UseOzonePlatform --ozone-platform=wayland >/dev/null 2>/dev/null &
         dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
+
+        ps -ef | rg "nata.sh" | sed -e "s/meow      //" | sed -e "s/ .*//" | xargs kill & # clean up previous nata processes
         ${flake_dir}/stuff/nata/nata.sh -c ${flake_dir}/stuff/nata/config.json >/dev/null 2>/dev/null &
 
       ''; # }}}
