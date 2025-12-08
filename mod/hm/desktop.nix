5e6caff6351b0e6f0333eb78ce8034bb29b826c8
nyxmeowmeow <rraaiin@proton.me>
2025-12-08 23:02:53 +1000

    08/12 23:02:53

--- mod/hm/desktop.nix
+++ mod/hm/desktop.nix
@@ -14,7 +14,7 @@ in {
     melee = {
       name = "melee";
       comment = "melee bash script";
-      exec = "${flake_dir}/stuff/scripts/melee.sh";
+      exec = "foot ${flake_dir}/stuff/scripts/melee.sh";
       icon = icon_dir + "/slippi.svg";
       # terminal = true;
     };
