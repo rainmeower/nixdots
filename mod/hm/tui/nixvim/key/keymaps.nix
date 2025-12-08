b76837464d85d09afc02514eb575b98c7f14d5e1
nyxmeowmeow <rraaiin@proton.me>
2025-12-09 06:45:16 +1000

    09/12 06:45:16

--- mod/hm/tui/nixvim/key/keymaps.nix
+++ mod/hm/tui/nixvim/key/keymaps.nix
@@ -12,14 +12,15 @@
     { # down in lists
       key = "<C-a>";
       action = "<C-n>";
-      mode = "c";
+      mode = [ "c" "i" ];
     }
-
     { # up in lists
       key = "<C-e>";
       action = "<C-p>";
-      mode = "c";
+      mode = [ "c" "i" ];
     }
+    { key = "up"; action = ""; mode = "i"; }
+    { key = "down"; action = ""; mode = "i"; }
 
     {
       key = "ga";
