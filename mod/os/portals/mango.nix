9e16d7d75b9450285607af11a701a32fff2ad22f
nyxmeowmeow <rraaiin@proton.me>
2025-12-10 04:59:02 +1000

    10/12 04:59:02

--- mod/os/portals/mango.nix
+++ mod/os/portals/mango.nix
@@ -44,6 +44,7 @@
         xdg-desktop-portal
         xdg-desktop-portal-wlr
         xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
         xdg-desktop-portal-termfilechooser
       ];
     };
