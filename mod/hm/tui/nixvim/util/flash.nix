9673ea705c12363f833377aaf06bd0b88bfe16d0
nyxmeowmeow <rraaiin@proton.me>
2025-12-09 13:31:59 +1000

    09/12 13:31:59

--- mod/hm/tui/nixvim/util/flash.nix
+++ mod/hm/tui/nixvim/util/flash.nix
@@ -4,9 +4,21 @@
       enable = true;
 
       lazyLoad.enable = true; # {{{
-      lazyLoad.settings.event = [
-        "UIEnter"
-      ]; # }}}
+      # lazyLoad.settings.event = [
+      #   "UIEnter"
+      # ]; # }}}
+
+      lazyLoad.settings = {
+      cmd.__raw = "function() require('flash').jump() end";
+    keymaps = [
+    {
+      __unkeyed-1 = "<cr>";
+      __unkeyed-3.__raw = "function() require('flash').jump() end";
+      mode = [ "n" "v" "x" "o" ];
+      desc = "jump with flash";
+    }
+    ];
+      };
 
       settings = {
         label.uppercase = false;
@@ -17,12 +29,5 @@
         };
       };
     };
-    keymaps = [
-    {
-      key = "<cr>";
-      action.__raw = "function() require('flash').jump() end";
-      mode = [ "n" "v" "x" "o" ];
-    }
-    ];
   };
 }
