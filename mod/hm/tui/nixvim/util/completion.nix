abc2666ea65bc082a67d5972603662b788824211
nyxmeowmeow <rraaiin@proton.me>
2025-12-09 13:24:20 +1000

    09/12 13:24:20

--- mod/hm/tui/nixvim/util/completion.nix
+++ mod/hm/tui/nixvim/util/completion.nix
@@ -1,39 +1,39 @@
 {
-  nv.plugins.cmp = {
-    enable = true;
-
-    settings = {
-      sources = [
-      { name = "nvim_lsp"; }
-      { name = "buffer"; }
-      { name = "path"; }
-      ];
-
-      preselect = # lua
-        "cmp.PreselectMode.None";
-      performance.max_view_entries = 5;
-      mapping = {
-# "<C-j>" = # lua
-#   "cmp.mapping.scroll_docs(-4)";
-# "<C-k>" = # lua
-#   "cmp.mapping.scroll_docs(4)";
-# "<C-Space>" = # lua
-# "cmp.mapping.complete()";
-# "<C-h>" = # lua
-#   "cmp.mapping.close()";
-# "<Tab>" = # lua
-#   "cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})";
-# "<C-i>" = # lua
-#   "cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})  ";
-# "<C-u>" = # lua
-#   "cmp.mapping(cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})";
-        "<C-i>" = # lua
-          "cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })";
-      };
-
-
-    };
-  };
+#   nv.plugins.cmp = {
+#     enable = true;
+#
+#     settings = {
+#       sources = [
+#       { name = "nvim_lsp"; }
+#       { name = "buffer"; }
+#       { name = "path"; }
+#       ];
+#
+#       preselect = # lua
+#         "cmp.PreselectMode.None";
+#       performance.max_view_entries = 5;
+#       mapping = {
+# # "<C-j>" = # lua
+# #   "cmp.mapping.scroll_docs(-4)";
+# # "<C-k>" = # lua
+# #   "cmp.mapping.scroll_docs(4)";
+# # "<C-Space>" = # lua
+# # "cmp.mapping.complete()";
+# # "<C-h>" = # lua
+# #   "cmp.mapping.close()";
+# # "<Tab>" = # lua
+# #   "cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})";
+# # "<C-i>" = # lua
+# #   "cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})  ";
+# # "<C-u>" = # lua
+# #   "cmp.mapping(cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})";
+#         "<C-i>" = # lua
+#           "cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })";
+#       };
+#
+#
+#     };
+#   };
 # nv.plugins.mini.modules.completion = {
 #   enable = true;
 #
