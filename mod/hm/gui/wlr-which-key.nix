b6e840d206653b56db5e8052a306bdfef76f3430
nyxmeowmeow <rraaiin@proton.me>
2025-12-11 13:29:10 +1000

    11/12 13:29:10

--- mod/hm/gui/wlr-which-key.nix
+++ mod/hm/gui/wlr-which-key.nix
@@ -21,7 +21,7 @@ menu:
     desc: quit
     cmd: ${if (wm == "hyprland") then "hyprctl dispatch exit"
            else if (wm == "niri") then "niri msg action quit"
-           else if (wm == "mango") then "${flake_dir}/stuff/scripts/keys/quit.sh mango"
+           else if (wm == "mango") then "foot --app-id='foot.quit_prompt' ${flake_dir}/stuff/scripts/keys/quit.sh mango"
            else err
           }
 
--- stuff/scripts/keys/quit.sh
+++ stuff/scripts/keys/quit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 should_quit="$(echo "quit $1?
 yes
-no" | fzf --no-input +s --style=minimal --tac)"
+no" | fzf --no-input +s --style=minimal --tac')"
 
 if [[ $should_quit == "yes" || $should_quit == "quit"* ]]; then 
   uwsm stop
