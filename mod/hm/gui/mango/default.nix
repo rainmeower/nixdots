2327981790bd2777dae9d30e418ac21d4e8ed038
nyxmeowmeow <rraaiin@proton.me>
2025-12-13 03:51:39 +1000

    13/12 03:51:39

--- mod/hm/gui/mango/default.nix
+++ mod/hm/gui/mango/default.nix
@@ -209,7 +209,7 @@ in {
             # reload config
             "SUPER+ALT,r,reload_config"
 
-            "SUPER,n,focusmon l"
+            "SUPER,n,focusmon DPa"
 
             # menu and terminal
             "SUPER,space,spawn,vicinae toggle"
