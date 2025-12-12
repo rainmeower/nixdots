55253850ed501030f5ec7d2e097599614a382f11
nyxmeowmeow <rraaiin@proton.me>
2025-12-12 20:50:21 +1000

    12/12 20:50:21

--- mod/hm/gui/mango/default.nix
+++ mod/hm/gui/mango/default.nix
@@ -104,6 +104,9 @@ in {
         animation_curve_close = "0.22, 0.94, 0.23, 1";
         animation_curve_tag   = "0.46, 1.0,  0.29, 1";
         animation_curve_focus = "0.46, 1.0,  0.29, 1";
+        # TODO
+        # animation_curve_opafadein  = 
+        # animation_curve_opafadeout = 
 
         # Scroller Layout Setting
         scroller_structs = gaps;
