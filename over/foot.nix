432a95d4049f4bdc5c2c6fd6f778ecaaa45a0707
nyxmeowmeow <rraaiin@proton.me>
2025-12-14 09:59:24 +1000

    14/12 09:59:24

--- over/foot.nix
+++ over/foot.nix
@@ -1,7 +1,8 @@
 (final: prev: {
   foot = prev.foot.overrideAttrs (old: {
     mesonBuildFlags = [
-      "-Dc_args=-O3 -march=native -pipe"
+      # "-Dc_args=-O3 -march=native -pipe"
+      "-Dc_args=-Ofast -march=native -pipe" # TODO
     ];
   });
 })
