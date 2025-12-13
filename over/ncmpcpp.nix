c3cee48fa279c555c3a077b37b2dfb83623b015e
nyxmeowmeow <rraaiin@proton.me>
2025-12-14 07:19:45 +1000

    14/12 07:19:45

--- over/ncmpcpp.nix
+++ over/ncmpcpp.nix
@@ -1,8 +1,26 @@
 (final: prev: {
-  ncmpcpp = prev.ncmpcpp.override {
+  # ncmpcpp = prev.ncmpcpp.override {
+  #   outputsSupport = false;
+  #   visualizerSupport = false;
+  #   clockSupport = false;
+  #   taglibSupport = false;
+  # };
+
+
+  ncmpcpp = (prev.ncmpcpp.overrideAttrs {
+    prePatch = ''
+    substituteInPlace src/status.cpp --replace-fail \
+    'Paused:' \
+    "ps:"
+
+    substituteInPlace src/status.cpp --replace-fail \
+    'Playing:' \
+    "pl:"
+    '';
+  }).override {
     outputsSupport = false;
     visualizerSupport = false;
     clockSupport = false;
     taglibSupport = false;
-  };
+   };
 })
