8468dc1854b07e9b2f2d390c79e6a63b2bbd1a34
nyxmeowmeow <rraaiin@proton.me>
2025-12-14 07:31:49 +1000

    14/12 07:31:49

--- over/ncmpcpp.nix
+++ over/ncmpcpp.nix
@@ -1,21 +1,12 @@
 (final: prev: {
-  # ncmpcpp = prev.ncmpcpp.override {
-  #   outputsSupport = false;
-  #   visualizerSupport = false;
-  #   clockSupport = false;
-  #   taglibSupport = false;
-  # };
-
-
   ncmpcpp = (prev.ncmpcpp.overrideAttrs {
     prePatch = ''
-    substituteInPlace src/status.cpp --replace-fail \
-    'Paused:' \
-    "ps:"
-
-    substituteInPlace src/status.cpp --replace-fail \
-    'Playing:' \
-    "pl:"
+      substituteInPlace src/status.cpp --replace-fail \
+      'Paused:' \
+      "ps:"
+      substituteInPlace src/status.cpp --replace-fail \
+      'Playing:' \
+      "pl:"
     '';
   }).override {
     outputsSupport = false;
--- over/yazi.nix
+++ over/yazi.nix
@@ -1,8 +1,11 @@
 (final: prev: {
-  # yazi = prev.yazi.overrideAttrs (old: {
-  #   CARGO_PROFILE_RELEASE_LTO = "true";
-  #   CARGO_PROFILE_RELEASE_OPT_LEVEL = "3";
-  #   CARGO_PROFILE_RELEASE_CODEGEN_UNITS = "1";
-  #   RUSTFLAGS = "-C target-cpu=native";
-  # });
+  yazi = prev.yazi.overrideAttrs {
+    CARGO_PROFILE_RELEASE_LTO = "fat";
+    CARGO_PROFILE_RELEASE_OPT_LEVEL = "3";
+    CARGO_PROFILE_RELEASE_CODEGEN_UNITS = "1";
+    CARGO_PROFILE_RELEASE_PANIC = "abort";
+    CARGO_PROFILE_RELEASE_STRIP = "symbols";
+    CARGO_PROFILE_RELEASE_DEBUG = false;
+    RUSTFLAGS = "-C target-cpu=native -Z threads=16";
+  };
 })
