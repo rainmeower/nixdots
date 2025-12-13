bdc9afc39df025161255cde660d11e76e8aaeb37
nyxmeowmeow <rraaiin@proton.me>
2025-12-14 08:12:33 +1000

    14/12 08:12:33

--- over/default.nix
+++ over/default.nix
@@ -14,6 +14,7 @@
   (import ./nushell.nix)
   (import ./nvim.nix)
   (import ./yazi.nix)
+  (import ./mpv.nix)
   (import ./ripgrep.nix)
 
   (import ./vicinae-extension-nix.nix)
--- over/mpv.nix
+++ over/mpv.nix
@@ -1,9 +1,7 @@
 (final: prev: {
-  mpv = (prev.mpv.overrideAttrs {
+  mpv = prev.mpv.overrideAttrs {
     mesonBuildFlags = [
       "-Dc_args=-O3 -march=native -pipe"
     ];
-  }).override {
-    vapoursynthSupport = false;
   };
 })
