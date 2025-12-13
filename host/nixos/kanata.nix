0b3d1cf8345ec787211f59cbbe0eac6056405919
nyxmeowmeow <rraaiin@proton.me>
2025-12-14 05:28:40 +1000

    14/12 05:28:40

--- host/nixos/kanata.nix
+++ host/nixos/kanata.nix
@@ -1137,7 +1137,7 @@ i i
 (deflayermap action ;; {{{
 f (multi (layer-switch base)
   ;; (cmd /etc/profiles/per-user/meow/bin/mmsg -d togglefloating))
-  (cmd bash -c 'sudo -u meow XDG_RUNTIME_DIR=/run/user/1000 /etc/profiles/per-user/meow/bin/mmsg -d togglefloating'))
+  (cmd bash -c 'XDG_RUNTIME_DIR=/run/user/1000 /etc/profiles/per-user/meow/bin/mmsg -d togglefloating'))
 
 ) ;; }}}
 
