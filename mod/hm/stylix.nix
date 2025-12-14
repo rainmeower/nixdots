5562f8c992dd4babc08076ab857f6bad8ec70b18
nyxmeowmeow <rraaiin@proton.me>
2025-12-15 09:08:00 +1000

    15/12 09:08:00

--- mod/hm/stylix.nix
+++ mod/hm/stylix.nix
@@ -3,26 +3,54 @@
   p,
   lib,
   ...
-}:{
-  # imports = [
-  #   inputs.stylix.homeModules.stylix
-  # ];
-  #
-  # stylix = {
-  #   enable = true;
-  #
-  #   autoEnable = false;
-  #   targets = {
-  #     gtk.enable = true;
-  #     gtk.colors.enable = true;
-  #     gtk.extraCss = "window.background { border-radius: 0; }";
-  #     qt.enable = true;
-  #   };
-  #
-  #   opacity = {
-  #     desktop = p.bg_opacity;
-  #     terminal = p.bg_opacity;
-  #   };
-  #
-  # };
+}: let
+  rh = lib.removePrefix "#";
+in {
+  imports = [
+    inputs.stylix.homeModules.stylix
+  ];
+
+  stylix = {
+    enable = true;
+
+    autoEnable = false;
+    targets = {
+      gtk.enable = true;
+      gtk.colors.enable = true;
+      gtk.extraCss = "window.background { border-radius: 0; }";
+      qt.enable = true;
+    };
+
+    opacity = {
+      desktop = p.bg_opacity;
+      terminal = p.bg_opacity;
+    };
+
+    base16Scheme = {
+      system = "base16";
+      name = "meow";
+      author = "nyx";
+      variant = "dark";
+
+      palette = {
+        base00 = rh p.bg;
+        base01 = rh p.bg;
+        base02 = rh p.bg;
+        base03 = rh p.bg;
+        base04 = rh p.grey;
+        base05 = rh p.muted;
+        base06 = rh p.comment;
+        base07 = rh p.fg;
+
+        base08 = rh p.red;
+        base09 = rh p.orange;
+        base0A = rh p.yellow;
+        base0B = rh p.green;
+        base0C = rh p.cyan;
+        base0D = rh p.blue;
+        base0E = rh p.purple;
+        base0F = rh p.pink;
+      };
+    };
+  };
 }
