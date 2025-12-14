197349c3b29e77fdae94ec944fd5fbaa42de99e0
nyxmeowmeow <rraaiin@proton.me>
2025-12-15 07:23:34 +1000

    15/12 07:23:34

--- mod/os/stylix.nix
+++ mod/os/stylix.nix
@@ -1,7 +1,11 @@
 {
   inputs,
+  self,
+  config,
   ...
-}:{
+}: let
+  p = import (self + /theme/${builtins.elemAt (builtins.split "_" config.theme) 0}/colors.nix);
+in {
   imports = [
     inputs.stylix.nixosModules.stylix
   ];
@@ -11,6 +15,38 @@
     targets = {
       gtk.enable = true;
       qt.enable = true;
+    };
+
+    base16Scheme = {
+      rosewater = "#f4dbd6";
+      flamingo = "#f0c6c6";
+      pink = "#f5bde6";
+      mauve = "#c6a0f6";
+      red = "#ed8796";
+      maroon = "#ee99a0";
+      peach = "#f5a97f";
+      yellow = "#eed49f";
+      green = "#a6da95";
+      teal = "#8bd5ca";
+      sky = "#91d7e3";
+      sapphire = "#7dc4e4";
+      blue = "#8aadf4";
+      lavender = "#b7bdf8";
+      text = "#cad3f5";
+      subtext1 = "#b8c0e0";
+      subtext0 = "#a5adcb";
+      subtext = "#a5adcb";
+      overlay2 = "#939ab7";
+      overlay1 = "#8087a2";
+      overlay0 = "#6e738d";
+      overlay = "#6e738d";
+      surface2 = "#5b6078";
+      surface1 = "#494d64";
+      surface0 = "#363a4f";
+      surface = "#363a4f";
+      base = "#24273a";
+      mantle = "#1e2030";
+      crust = "#181926";
     };
   };
 }
