8bd741d1b762c7fa076c52156630e46efb9c4191
nyxmeowmeow <rraaiin@proton.me>
2025-12-14 10:17:55 +1000

    14/12 10:17:55

---
+++ over/uutils.nix
@@ -0,0 +1,15 @@
+(final: prev: {
+  uutils-coreutils-noprefix = prev.uutils-coreutils-noprefix.overrideAttrs (old: {
+
+    CARGO_PROFILE_RELEASE_LTO = "fat";
+    CARGO_PROFILE_RELEASE_OPT_LEVEL = "3";
+    CARGO_PROFILE_RELEASE_CODEGEN_UNITS = "1";
+    CARGO_PROFILE_RELEASE_PANIC = "abort";
+    CARGO_PROFILE_RELEASE_STRIP = "symbols";
+    CARGO_PROFILE_RELEASE_DEBUG = false;
+
+    env = (old.env or {}) // {
+      RUSTFLAGS = "-C target-cpu=native -Z threads=16";
+    };
+  });
+})
