61663e3bf518a38eca3af1fddf90667286bb3ccf
nyxmeowmeow <rraaiin@proton.me>
2025-12-15 15:56:18 +1000

    15/12 15:56:18

--- mod/hm/tui/skim.nix
+++ mod/hm/tui/skim.nix
@@ -19,5 +19,5 @@
     # ];
   };
 
-  home.sessionVariables.SKIM_DEFAULT_OPTS = "--prompt='${prompt_sym} ',--cmd-prompt='$$${prompt_sym} ',--tabstop=4,--no-mouse,--color=fg:${p.fg},hl:${p.accent},fg+:${p.bg},bg+:${p.accent},hl+:${p.bg},info:#ff0000,prompt:${p.accent},spinner:-1,pointer:-1,gutter:-1,border:-1";
+  home.sessionVariables.SKIM_DEFAULT_OPTIONS = "--prompt='${prompt_sym} ',--cmd-prompt='$$${prompt_sym} ',--tabstop=4,--no-mouse,--color=fg:${p.fg},hl:${p.accent},fg+:${p.bg},bg+:${p.accent},hl+:${p.bg},info:#ff0000,prompt:${p.accent},spinner:-1,pointer:-1,gutter:-1,border:-1";
 }
