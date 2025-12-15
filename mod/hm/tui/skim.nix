03dbef0652d31e1b2d91f753aecfe9a37e093305
nyxmeowmeow <rraaiin@proton.me>
2025-12-15 15:53:17 +1000

    15/12 15:53:17

--- mod/hm/tui/skim.nix
+++ mod/hm/tui/skim.nix
@@ -9,13 +9,15 @@
     # enableNushellIntegration = true;
 
     defaultCommand = "fd";
-    defaultOptions = [
-      "--prompt='${prompt_sym} '"
-      "--cmd-prompt='$$${prompt_sym} '"
-      "--tabstop=4"
-      "--no-mouse"
-
-      "--color=fg:${p.fg},hl:${p.accent},fg+:${p.bg},bg+:${p.accent},hl+:${p.bg},info:#ff0000,prompt:${p.accent},spinner:-1,pointer:-1,gutter:-1,border:-1"
-    ];
+    # defaultOptions = [
+    #   "--prompt='${prompt_sym} '"
+    #   "--cmd-prompt='$$${prompt_sym} '"
+    #   "--tabstop=4"
+    #   "--no-mouse"
+    #
+    #   "--color=fg:${p.fg},hl:${p.accent},fg+:${p.bg},bg+:${p.accent},hl+:${p.bg},info:#ff0000,prompt:${p.accent},spinner:-1,pointer:-1,gutter:-1,border:-1"
+    # ];
   };
+
+  home.sessionVariables.SKIM_DEFAULT_OPTS = "--prompt='${prompt_sym} ',--cmd-prompt='$$${prompt_sym} ',--tabstop=4,--no-mouse,--color=fg:${p.fg},hl:${p.accent},fg+:${p.bg},bg+:${p.accent},hl+:${p.bg},info:#ff0000,prompt:${p.accent},spinner:-1,pointer:-1,gutter:-1,border:-1";
 }
