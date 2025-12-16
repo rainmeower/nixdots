4265a963b02c53a777f45c774fdae9a6bd6c9d2e
nyxmeowmeow <rraaiin@proton.me>
2025-12-16 18:45:14 +1000

    16/12 18:45:14

--- mod/hm/gui/mango/default.nix
+++ mod/hm/gui/mango/default.nix
@@ -30,8 +30,8 @@ bind=NONE,${key},${action}
 bind=SUPER,${key},${action}
   '';
   media = key: action: ''
-bind=NONE,${key},spawn,${action} && mmsg -d setkeymode default
-bind=SUPER,${key},spawn,${action} && mmsg -d setkeymode default
+bind=NONE,${key},spawn,sh -c '${action} && mmsg -d setkeymode default'
+bind=SUPER,${key},spawn,sh -c '${action} && mmsg -d setkeymode default'
   '';
   media_stay = key: action: ''
 bind=NONE,${key},spawn,${action}
