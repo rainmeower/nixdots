49215a15190f21b9e6e758926cc0fb7b399f188e
nyxmeowmeow <rraaiin@proton.me>
2025-12-16 18:20:34 +1000

    16/12 18:20:34

--- mod/hm/gui/mango/default.nix
+++ mod/hm/gui/mango/default.nix
@@ -366,7 +366,8 @@ in {
           # momentum mod
           "appid:steam_app_1802710,force_tearing:1,isfullscreen:0,noblur:1,isfloating:0,width:2560,height:1080"
 
-          "appid:swayimg,appid:mpv,animation_type_open:none,animation_type_clone:none"
+          "appid:swayimg,animation_type_open:none,animation_type_clone:none"
+          "appid:mpv,animation_type_open:none,animation_type_clone:none"
           "appid:equibop,appid:vesktop,tags:2,isopensilent:1"
           # TODO `isterm` + emacs/yazi?
         ]; # }}}
