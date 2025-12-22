a49dbf92614e9ef4e59f2fd308f57d126b559bc8
nyxmeowmeow <rraaiin@proton.me>
2025-12-22 13:18:59 +1000

    22/12 13:18:59

--- mod/hm/tui/clipse.nix
+++ mod/hm/tui/clipse.nix
@@ -1,27 +1,29 @@
 {
   pkgs,
+  p,
   ...
 }:{
-  # services.clipse = {
-  # package = pkgs.my-clipse;
-  #   imageDisplay = {
-  #     type = "sixel";
-  #   };
-  #   theme = {
-  #     useCustomTheme = true;
-  #     DimmedDesc = "#a5adcb";
-  #     DimmedTitle = "#a5adcb";
-  #     FilteredMatch = "#cad3f5";
-  #     NormalDesc = "#000000";
-  #     NormalTitle = "#cad3f5";
-  #     SelectedDesc = "#000000";
-  #     SelectedTitle = "#ffffff";
-  #     SelectedBorder = "#00000000";
-  #     SelectedDescBorder = "#00000000";
-  #     TitleFore = "#cad3f5";
-  #     Titleback = "#00000000";
-  #     StatusMsg = "#ffffff";
-  #     PinIndicatorColor = "#b7bdf8";
-  #   };
-  # };
+  services.clipse = {
+    # package = pkgs.my-clipse;
+    imageDisplay = {
+      type = "sixel";
+    };
+
+    theme = {
+      useCustomTheme = true;
+      DimmedDesc = p.comment;
+      DimmedTitle = p.comment;
+      FilteredMatch = p.accent;
+      NormalDesc = "#000000";
+      NormalTitle = p.fg;
+      SelectedDesc = "#000000";
+      SelectedTitle = "#ffffff";
+      SelectedBorder = "#00000000";
+      SelectedDescBorder = "#00000000";
+      TitleFore = p.fg;
+      Titleback = "#00000000";
+      StatusMsg = "#ffffff";
+      PinIndicatorColor = p.accent;
+    };
+  };
 }
--- mod/os/clipse.nix
+++ mod/os/clipse.nix
@@ -3,32 +3,32 @@
   lib,
   ...
 }:{
-  # nixpkgs.overlays = [
-  #   (final: prev: {
-  #     my-clipse = prev.buildGoModule {
-  #       pname = "clipse";
-  #       version = "9.9.9";
-  #
-  #       src = prev.fetchFromGitHub {
-  #         owner = "savedra1";
-  #         repo = "clipse";
-  #         rev = "ebb2810812aad967e86d269cf14d413eb5f2dc9e";
-  #         hash = "sha256-6UqdhWvitfHSDmiBa98Sn4PDvbH9shugjc0o4pyU52w=";
-  #       };
-  #
-  #       vendorHash = "sha256-ANMeYiN+66F8EF7dB8+zuQyuT3moSuNxX8+vQ9GH2w4=";
-  #
-  #       meta = {
-  #         description = "Configurable TUI clipboard manager for Unix ";
-  #         homepage = "https://github.com/savedra1/clipse";
-  #         license = lib.licenses.mit;
-  #         maintainers = [ lib.maintainers.savedra1 ];
-  #       };
-  #     };
-  #   })
-  # ];
-  #
-  # environment.systemPackages = with pkgs; [
-  #   clipse
-  # ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      my-clipse = prev.buildGoModule {
+        pname = "clipse";
+        version = "9.9.9";
+
+        src = prev.fetchFromGitHub {
+          owner = "savedra1";
+          repo = "clipse";
+          rev = "ea4c0e1ca61881f5736ae18574dc64ed793df80f";
+          hash = "";
+        };
+
+        vendorHash = "";
+
+        meta = {
+          description = "Configurable TUI clipboard manager for Unix ";
+          homepage = "https://github.com/savedra1/clipse";
+          license = lib.licenses.mit;
+          maintainers = [ lib.maintainers.savedra1 ];
+        };
+      };
+    })
+  ];
+
+  environment.systemPackages = with pkgs; [
+    clipse
+  ];
 }
