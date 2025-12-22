bd72a344c5552ed016e01d90d971d2f4a087e619
nyxmeowmeow <rraaiin@proton.me>
2025-12-22 22:34:32 +1000

    22/12 22:34:32

--- over/gurk.nix
+++ over/gurk.nix
@@ -4,18 +4,26 @@
       substituteInPlace src/ui/draw.rs --replace-fail \
       '.title("Channels")' \
       ""
+
       substituteInPlace src/ui/draw.rs --replace-fail \
       '.title("Messages")' \
       '.title("")'
+
       substituteInPlace src/ui/draw.rs --replace-fail \
       '"Messages".to_string()' \
       '"".to_string()'
+
       substituteInPlace src/ui/draw.rs --replace-fail \
       'format!("Messages {' \
       'format!("{'
+
       substituteInPlace src/ui/draw.rs --replace-fail \
       '=> "Input"' \
       '=> ""'
+
+      substituteInPlace src/app.rs --replace-fail \
+      '[{}] writing...' \
+      '╴[{}] typing...╶'
 
       # TODO change writing... to typing...
     '';
