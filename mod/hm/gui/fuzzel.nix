b75c4f6b32e6e4b6f623924a15a9faf1dc7c5783
nyxmeowmeow <rraaiin@proton.me>
2025-12-22 14:06:03 +1000

    22/12 14:06:03

--- mod/hm/gui/fuzzel.nix
+++ mod/hm/gui/fuzzel.nix
@@ -1,12 +1,16 @@
 {
   pkgs,
+  lib,
   p,
   font_path,
   font_name,
   prompt_sym,
   border_width,
+  rounding,
   ...
-}:{
+}: let
+  radius = if rounding then 10 else 0;
+in {
   programs.fuzzel = {
     enable = true;
 
@@ -16,28 +20,30 @@
         font = font_name + ":size=22";
         terminal = "${pkgs.foot}/bin/foot";
         layer = "overlay";
-        prompt = prompt_sym;
+        prompt = prompt_sym + " ";
         hide-before-typing = true;
         lines = 5;
         dpi-aware = false;
       };
 
 
-      colors = {
-        background = p.bg_trans;
-        text = p.fg;
-        input = p.fg;
-        prompt = p.accent;
-        border = p.accent;
-        selection = p.accent;
-        selection-text= p.bg;
-        match = p.accent;
-        selection-match = p.bg;
+      colors = let
+        rh = lib.removePrefix "#";
+      in {
+        background = rh p.bg_trans;
+        text = rh p.fg;
+        input = rh p.fg;
+        prompt = rh p.accent;
+        border = rh p.accent;
+        selection = rh p.accent;
+        selection-text= rh p.bg;
+        match = rh p.accent;
+        selection-match = rh p.bg;
       };
 
       border = {
         width = border_width;
-        radius = 10; # TODO
+        inherit radius; # TODO
       };
 
     };
