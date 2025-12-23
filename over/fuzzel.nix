240febe6490279b7a2c29b272315bdeebf09b95c
nyxmeowmeow <rraaiin@proton.me>
2025-12-24 02:58:30 +1000

    24/12 02:58:30

--- over/fuzzel.nix
+++ over/fuzzel.nix
@@ -2,7 +2,7 @@
   fuzzel = prev.fuzzel.overrideAttrs {
     mesonBuildFlags = [
       # "-Dc_args=-O3 -march=native -pipe"
-      "-Dc_args=-O3 -march=native -pipe" # TODO see if Ofast breaks it
+      "-Dc_args=-Ofast -march=native -pipe" # TODO see if Ofast breaks it
     ];
   };
 })
