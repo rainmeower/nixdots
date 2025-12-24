857ce2cad399fb17798c9f8950c60968f25d144e
nyxmeowmeow <rraaiin@proton.me>
2025-12-24 11:28:07 +1000

    24/12 11:28:07

--- mod/hm/tui/zoxide.nix
+++ mod/hm/tui/zoxide.nix
@@ -2,7 +2,7 @@
   programs.zoxide = {
     enable = true;
     enableBashIntegration = true;
-    enableNushellIntegration = false;
+    enableNushellIntegration = true;
     options = [
       "--cmd c"
     ];
