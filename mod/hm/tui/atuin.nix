cb29817e502c7d43ded8abdd00e3531ebfaae325
nyxmeowmeow <rraaiin@proton.me>
2025-12-26 13:28:28 +1000

    26/12 13:28:28

--- mod/hm/tui/atuin.nix
+++ mod/hm/tui/atuin.nix
@@ -11,6 +11,36 @@
     settings = {
       auto_sync = false;
 
+      stats = {
+        # Set commands where we should consider the subcommand for statistics. Eg kubectl get vs just kubectl
+        common_subcommands = [
+          "cargo"
+          "composer"
+          "docker"
+          "dotnet"
+          "git"
+          "go"
+          "ip"
+          "jj"
+          "kubectl"
+          "nix"
+          "nmcli"
+          "npm"
+          "pecl"
+          "pnpm"
+          "podman"
+          "port"
+          "systemctl"
+          "tmux"
+          "yarn"
+          "mmsg"
+          "niri"
+          "hyprctl"
+          "sudo"
+          "xargs"
+          "bmm"
+        ];
+      };
     };
   };
 }
