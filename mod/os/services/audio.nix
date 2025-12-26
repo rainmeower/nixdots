d164dd850f6bf5d376833936db4aeabd3d22ecb5
nyxmeowmeow <rraaiin@proton.me>
2025-12-26 15:01:05 +1000

    26/12 15:01:05

--- mod/os/services/audio.nix
+++ mod/os/services/audio.nix
@@ -20,7 +20,24 @@
     #     "min.quantum" = "256/48000 # 2.7ms";
     #   };
     # };
+
+
+    wireplumber.extraConfig = {
+      "headphones-rename" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [ { "node.name" = "alsa_output.usb-Kingston_HyperX_Virtual_Surround_Sound_00000000-00.analog-stereo"; } ];
+            actions = {
+              update-props = {
+                "node.nick" = "hyperx cloud ii";
+              };
+            };
+          }
+        ];
+      };
+    };
   };
+
 
   # make pipewire realtime-capable
   security.rtkit.enable = true;
