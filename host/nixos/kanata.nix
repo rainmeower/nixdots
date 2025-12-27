bc6c0212a7adeb645988274c1ebfac0262ec2d04
nyxmeowmeow <rraaiin@proton.me>
2025-12-27 16:25:26 +1000

    27/12 16:25:26

--- host/nixos/kanata.nix
+++ host/nixos/kanata.nix
@@ -179,7 +179,7 @@ mods:
   cw (caps-word-custom
     2000 ;; time before it cancels after no keypresses
     (a b c d e f g h i j k l m n o p q r s t u v w x y z) ;; words that get shifted
-    (bspc del up down left right - 0 1 2 3 4 5 6 7 8 9 lsft rsft) ;; words that stop the shifting
+    (bspc del up down left right 0 1 2 3 4 5 6 7 8 9 lsft rsft -) ;; words that stop the shifting
   )
 
   ;; TODO convert rust ints to c99 ints
