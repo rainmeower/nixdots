4109f08d6551aeb9907affe8eb32b135610d093b
nyxmeowmeow <rraaiin@proton.me>
2025-12-28 21:53:22 +1000

    28/12 21:53:22 remove fuzzel

--- mod/hm/gui/fuzzel.nix
+++ mod/hm/gui/fuzzel.nix
@@ -11,43 +11,43 @@
 }: let
   radius = if rounding then 10 else 0;
 in {
-  programs.fuzzel = {
-    enable = true;
-
-    settings = {
-      main = {
-        # font = font_path + ":size=22";
-        font = font_name + ":size=22";
-        terminal = "${pkgs.foot}/bin/foot";
-        layer = "overlay";
-        prompt = ''"${prompt_sym} "'';
-        hide-before-typing = false; # no way to disable this with a flag
-        lines = 5;
-        dpi-aware = false;
-        # gross
-        enable-mouse = false;
-      };
-
-      colors = let
-        rh = lib.removePrefix "#";
-      in rec {
-        # NOTE cannot omit alpha
-        background = rh p.bg_trans;
-        text = rh p.fg + "ff";
-        input = text;
-        selection-text= rh p.bg + "ff";
-        selection = rh p.accent + "ff";
-        border = selection;
-        prompt = selection;
-        match = selection;
-        selection-match = rh p.bg + "ff";
-        placeholder = rh p.comment + "ff";
-      };
-
-      border = {
-        width = border_width;
-        inherit radius; # TODO
-      };
-    };
-  };
+  # programs.fuzzel = {
+  #   enable = true;
+  #
+  #   settings = {
+  #     main = {
+  #       # font = font_path + ":size=22";
+  #       font = font_name + ":size=22";
+  #       terminal = "${pkgs.foot}/bin/foot";
+  #       layer = "overlay";
+  #       prompt = ''"${prompt_sym} "'';
+  #       hide-before-typing = false; # no way to disable this with a flag
+  #       lines = 5;
+  #       dpi-aware = false;
+  #       # gross
+  #       enable-mouse = false;
+  #     };
+  #
+  #     colors = let
+  #       rh = lib.removePrefix "#";
+  #     in rec {
+  #       # NOTE cannot omit alpha
+  #       background = rh p.bg_trans;
+  #       text = rh p.fg + "ff";
+  #       input = text;
+  #       selection-text= rh p.bg + "ff";
+  #       selection = rh p.accent + "ff";
+  #       border = selection;
+  #       prompt = selection;
+  #       match = selection;
+  #       selection-match = rh p.bg + "ff";
+  #       placeholder = rh p.comment + "ff";
+  #     };
+  #
+  #     border = {
+  #       width = border_width;
+  #       inherit radius; # TODO
+  #     };
+  #   };
+  # };
 }
