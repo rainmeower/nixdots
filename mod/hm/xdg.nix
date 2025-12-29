e8e0acae995cf36553b7b7b621b1d4437a498c6c
nyxmeowmeow <rraaiin@proton.me>
2025-12-30 05:23:58 +1000

    30/12 05:23:58

--- mod/hm/xdg.nix
+++ mod/hm/xdg.nix
@@ -11,13 +11,12 @@ in {
     enable = true;
     createDirectories = true;
 
-    inherit (dirs)
-      documents
-      music
-      pictures
-      videos
-    ;
+    /* default names are too long and have yucky capitalism */
     download  = h + "/dl"; # why doesnt this have an s at the end??
+    documents = h + "/doc";
+    music     = h + "/music";
+    pictures  = h + "/pic";
+    videos    = h + "/vid";
 
     /* dont need */
     publicShare = null;
@@ -25,8 +24,8 @@ in {
     desktop     = null;
 
     extraConfig = {
-      XDG_GAMES_DIR = dirs.games;
-      XDG_MISC_DIR  = dirs.misc;
+      XDG_GAMES_DIR = h + "/games";
+      XDG_MISC_DIR  = h + "/misc";
 
       /* idk why im setting these when theyre all default */
       XDG_CONFIG_HOME = h + "/.config";
@@ -35,3 +34,45 @@ in {
     };
   };
 }
+
+
+
+
+
+# {
+#   config,
+#   dirs,
+#   ...
+# }: let
+#   h = config.home.homeDirectory;
+# in {
+#   home.preferXdgDirectories = true;
+#   xdg.enable = true;
+#   xdg.userDirs = {
+#     enable = true;
+#     createDirectories = true;
+#
+#     inherit (dirs)
+#       documents
+#       music
+#       pictures
+#       videos
+#     ;
+#     download  = h + "/dl"; # why doesnt this have an s at the end??
+#
+#     /* dont need */
+#     publicShare = null;
+#     templates   = null;
+#     desktop     = null;
+#
+#     extraConfig = {
+#       XDG_GAMES_DIR = dirs.games;
+#       XDG_MISC_DIR  = dirs.misc;
+#
+#       /* idk why im setting these when theyre all default */
+#       XDG_CONFIG_HOME = h + "/.config";
+#       XDG_DATA_HOME   = h + "/.local/share";
+#       XDG_CACHE_HOME  = h + "/.cache";
+#     };
+#   };
+# }
